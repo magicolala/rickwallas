737b6d7fa835e05ab5971d46df709226c29ad1a5
Cédric Oloa <cedric.oloa@laposte.net>
2018-01-27 23:47:54 +0100

    bouton flex

--- js/dimension.js
+++ js/dimension.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // DIMENSIONS
     var window_width = $(window).width();
     var window_height = $(window).height();
     
@@ -6,4 +7,11 @@ $(document).ready(function(){
     
     $('.container-fluid').height(height_container);
     $('.menu-choice-music').height(height_container-200);
+    
+    // FEATURES    
+    $(document).on('click', '#chevron-choice-music', function() {
+        $('#menu-music').slideUp(700);
+        $('.fa-chevron-up').css('transform','rotate(180deg)');
+    });
 });
+
